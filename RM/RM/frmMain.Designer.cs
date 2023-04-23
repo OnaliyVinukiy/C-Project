2de1ad447848b096819b43c2e50484731403164d
avish <avishmadushanka21@gmail.com>
2023-04-23 12:12:13 +0530

    a

--- RM/RM/frmMain.Designer.cs
+++ RM/RM/frmMain.Designer.cs
@@ -70,11 +70,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Castellar", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Castellar", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(41, 116);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(183, 27);
+            this.label1.Size = new System.Drawing.Size(194, 27);
             this.label1.TabIndex = 2;
             this.label1.Text = "Family Chef";
             this.label1.Click += new System.EventHandler(this.label1_Click);
